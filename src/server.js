efbf6d4b2040bef93c1046e9f6580ba92eee1610
minhduc2001 <ngoduc2468@gmail.com>
2022-07-18 17:17:35 +0700

    change nodemon to node

--- src/server.js
+++ src/server.js
@@ -10,9 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({limit: '50mb'}));
-
-app.use(cookieParser());
 app.use(cors({credentials: true, origin: 'https://stream-e2870.web.app'}));
+app.use(cookieParser());
+
 app.use(morgan('dev'));
 router(app);
 
