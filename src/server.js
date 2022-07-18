65ea8f6b79c28b723585ec4feafb076cadd94d18
minhduc2001 <ngoduc2468@gmail.com>
2022-07-18 13:34:48 +0700

    change origin

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({limit: '50mb'}));
-app.use(cors({credentials: true, origin: 'https://music-app-frontend.herokuapp.com'}));
+app.use(cors({credentials: true, origin: 'https://stream-e2870.web.app'}));
 app.use(cookieParser());
 app.use(morgan('dev'));
 router(app);
