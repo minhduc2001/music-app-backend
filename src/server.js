2fbb3ce424dc1ccadee27d6408139e8ed4ea10e1
minhduc2001 <ngoduc2468@gmail.com>
2022-07-18 17:15:50 +0700

    origin

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({limit: '50mb'}));
 
 app.use(cookieParser());
-app.use(cors({credentials: true, origin: 'https://stream-e2870.firebaseapp.com'}));
+app.use(cors({credentials: true, origin: 'https://stream-e2870.web.app'}));
 app.use(morgan('dev'));
 router(app);
 
