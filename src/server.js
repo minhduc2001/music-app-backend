2075473263559e2a79e6fadd7243bb824e1a107a
minhduc2001 <ngoduc2468@gmail.com>
2022-07-18 11:59:16 +0700

    origin

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({limit: '50mb'}));
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cors({credentials: true, origin: 'https://music-app-frontend.herokuapp.com'}));
 app.use(cookieParser());
 app.use(morgan('dev'));
 router(app);
